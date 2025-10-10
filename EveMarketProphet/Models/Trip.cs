8494a115600700b172e7287fd597f148a55db90f
David van der Wulp <83947060+davidvdw02@users.noreply.github.com>
2025-10-10 03:30:30 +0200

    Add multi-leg journey planner

--- EveMarketProphet/Models/Trip.cs
+++ EveMarketProphet/Models/Trip.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using EveMarketProphet.Services;
 
 namespace EveMarketProphet.Models
@@ -13,5 +14,17 @@ namespace EveMarketProphet.Models
         public double ProfitPerJump { get; set; }
         public double Cost { get; set; }
         public double Weight { get; set; }
+
+        public Transaction PrimaryTransaction => Transactions?.FirstOrDefault();
+
+        public int StartSystemId => PrimaryTransaction?.StartSystemId ?? 0;
+        public int EndSystemId => PrimaryTransaction?.EndSystemId ?? 0;
+        public long StartStationId => PrimaryTransaction?.StartStationId ?? 0;
+        public long EndStationId => PrimaryTransaction?.EndStationId ?? 0;
+        public string StartStationName => PrimaryTransaction?.SellOrder?.StationName;
+        public string EndStationName => PrimaryTransaction?.BuyOrder?.StationName;
+
+        public IReadOnlyList<int> TradeWaypoints => PrimaryTransaction?.Waypoints;
+        public int TradeJumps => PrimaryTransaction?.Jumps ?? 0;
     }
 }
